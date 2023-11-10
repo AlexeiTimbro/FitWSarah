7514ffb7c6af7e39600839fda8fa16f5a13c83a8
Timbro <alexei.timbro@gmail.com>
2023-11-10 01:13:14 -0500

    First commit of the project test Alexei Timbro

---
+++ src/main/java/com/fitwsarah/fitwsarah/FitWSarahApplication.java
@@ -0,0 +1,13 @@
+package com.fitwsarah.fitwsarah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitWSarahApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitWSarahApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fitwsarah/fitwsarah/FitWSarahApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitwsarah.fitwsarah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitWSarahApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
