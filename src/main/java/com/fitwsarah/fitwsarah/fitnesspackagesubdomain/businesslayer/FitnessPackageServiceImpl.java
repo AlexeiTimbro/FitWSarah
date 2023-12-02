a8313d7f6be5aaa086cb83060d035da4183b5033
Timbro <alexei.timbro@gmail.com>
2023-12-01 22:39:06 -0500

    test

--- src/main/java/com/fitwsarah/fitwsarah/fitnesspackagesubdomain/businesslayer/FitnessPackageServiceImpl.java
+++ src/main/java/com/fitwsarah/fitwsarah/fitnesspackagesubdomain/businesslayer/FitnessPackageServiceImpl.java
@@ -1,7 +1,36 @@
 package com.fitwsarah.fitwsarah.fitnesspackagesubdomain.businesslayer;
 
+import com.fitwsarah.fitwsarah.fitnesspackagesubdomain.presentationlayer.FitnessPackageRequestModel;
+import com.fitwsarah.fitwsarah.fitnesspackagesubdomain.presentationlayer.FitnessPackageResponseModel;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class FitnessPackageServiceImpl {
+public class FitnessPackageServiceImpl implements FitnessPackageService{
+
+    @Override
+    public List<FitnessPackageResponseModel> getAllFitnessPackages() {
+        return null;
+    }
+
+    @Override
+    public FitnessPackageResponseModel getFitnessPackageByFitnessPackageId(String fitnessPackageId) {
+        return null;
+    }
+
+    @Override
+    public FitnessPackageResponseModel addFitnessPackage(FitnessPackageRequestModel fitnessPackageRequestModel, String fitnessPackageId) {
+        return null;
+    }
+
+    @Override
+    public FitnessPackageResponseModel updateFitnessPackage(FitnessPackageRequestModel fitnessPackageRequestModel, String fitnessPackageId) {
+        return null;
+    }
+
+    @Override
+    public void removeFitnessPackage(String fitnessPackageId) {
+
+    }
 }
