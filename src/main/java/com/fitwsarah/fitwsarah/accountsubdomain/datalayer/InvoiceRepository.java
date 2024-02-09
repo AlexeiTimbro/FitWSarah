ce73dc66e09f2456936d55bc6c2b406cd78f8495
Abdur Siddiqui <abdursiddiqui50@gmail.com>
2024-02-09 17:15:21 -0500

    removed unused methods

--- src/main/java/com/fitwsarah/fitwsarah/accountsubdomain/datalayer/InvoiceRepository.java
+++ src/main/java/com/fitwsarah/fitwsarah/accountsubdomain/datalayer/InvoiceRepository.java
@@ -10,10 +10,5 @@ public interface InvoiceRepository extends JpaRepository<Invoices, Integer> {
 
              List<Invoices> findInvoicesByUserId(String userId);
 
-    List<Invoices> findInvoicesByUserIdStartingWith(String userId);
-             List<Invoices> findInvoicesByInvoiceIdentifier_InvoiceIdStartingWith(String invoiceId);
-    List<Invoices> findAllInvoicesByUsernameStartingWith(String username);
-    List<Invoices> findInvoicesByStatus(InvoiceStatus status);
-    List<Invoices> findInvoicesByPaymentTypeStartingWith(String paymentType);
-    List<Invoices> findInvoicesByPrice(double price);
+
 }
